e0972fbdaac35afd99ebaa766deaaaccef5e06fd
EC2 Default User <ec2-user@ip-172-31-47-253.ap-northeast-1.compute.internal>
2024-11-22 11:07:18 +0000

    確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,8 @@
 Rails.application.routes.draw do
-   # トップ画面のルートを設定
-   get 'books' => 'books#index', as: :books
-
-   # 新規作成フォームを表示するためのルート
-   get 'books/new' => 'books#new', as: :new_book
- 
-   # データを送信する（作成用）
-   post 'books' => 'books#create'
- 
-   # 詳細ページを表示するためのルート
-   get 'books/:id' => 'books#show', as: :book
- 
-   # 編集フォームを表示するためのルート
-   get 'books/:id/edit' => 'books#edit', as: :edit_book
- 
-   # 更新処理のルート
-   patch 'books/:id' => 'books#update'
-   put 'books/:id' => 'books#update'
- 
-   # 削除処理のルート
-   delete 'books/:id' => 'books#destroy'
- 
-   # アプリケーションのトップページを books#index に設定
-   root 'books#index'
- end
- 
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books' => 'books#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
